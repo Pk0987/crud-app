382ecb558abb7e143b0893916d0e6402053222c1
Your Name <you@example.com>
2023-07-16 01:26:34 +0530

    crud-final

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../service/employee.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CoreServiceService } from '../core/core-service.service';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss']
+})
+export class EmpAddEditComponent implements OnInit {
+
+  empForm: FormGroup;
+
+  education : string[] = [
+    'Metric','Intermediate','Diploma','Graduate', 'Post Graduate',
+  ];
+
+  constructor(
+     private _fb : FormBuilder,
+     private _empService: EmployeeService, 
+     private _dialogRef : MatDialogRef<EmpAddEditComponent>,
+     @Inject(MAT_DIALOG_DATA) public data : any,
+     private coreService : CoreServiceService,
+     ) {
+    this.empForm = this._fb.group({
+      firstName :'',
+      lastName :'',
+      email :'',
+      dob :'',
+      gender :'',
+      education :'',
+      company :'',
+      experience :'',
+      package: '',
+    });
+  }
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);   
+  }
+   
+  onFormSubmit(){
+    if(this.empForm.valid){
+      if(this.data){
+        this._empService.updateEmployee(this.data.id, this.empForm.value).subscribe({
+          next:(val:any)=>{
+             this.coreService.openSnackBar('Updated Successfully..!',)
+             this._dialogRef.close(true);
+          },
+          error:(err:any)=>{
+            console.error(err);
+          }
+        })
+      }
+      else{
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next:(val:any)=>{
+            this.coreService.openSnackBar('Employee details added Successfully..!', 'Done')
+            this._dialogRef.close(true);
+          },
+          error:(err:any)=>{
+            console.error(err);
+          }
+        })
+      }
+    }
+  }
+}
